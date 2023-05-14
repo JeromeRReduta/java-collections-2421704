34de8d41baa75b233b4358db1144c80254624212
Jerome Reduta <JRReduta@yahoo.com>
2023-05-13 20:07:09 -0700

    Finished 2-10

--- src/main/java/com/linkedin/collections/RoomService.java
+++ src/main/java/com/linkedin/collections/RoomService.java
@@ -1,29 +1,32 @@
 package com.linkedin.collections;
 
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.HashSet;
+import java.util.*;
 
 public class RoomService {
 
 	private Collection<Room> inventory;
 
 	public RoomService() {
-		this.inventory = new HashSet<>();
+		this.inventory = new LinkedHashSet<>();
 	}
 	
 	public boolean hasRoom(Room room) {
 		
 	// 1. Returns a boolean that indicates if the Room Inventory contains a Room.
 		
-		return false;
+		return inventory.contains(room);
 	}
 	
 	public Room[] asArray() {
 		
 	// 2. Returns all Rooms as an Array of Rooms in the **order** they were Added.
 		
-		return null;
+		Room[] array = new Room[inventory.size()];
+		int i = 0;
+		for (Room room : inventory) {
+			array[i++] = room;
+		}
+		return array;
 	}
 	
 	public Collection<Room> getByType(String type){
@@ -33,8 +36,14 @@ public class RoomService {
 		  The original Room Inventory collection MUST NOT BE MODIFIED.
 	*/
 		
-		return null;
-		
+		Collection<Room> matching = new ArrayList<>();
+		inventory.forEach(room -> {
+					boolean isSameType = room.getType().equals(type);
+					if (isSameType) {
+						matching.add(room);
+					}
+				});
+		return matching;
 	}
 
 	public Collection<Room> getInventory() {
